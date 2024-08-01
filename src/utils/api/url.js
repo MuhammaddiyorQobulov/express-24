f85f387d47c01bdd0b0be3ad0a55f2365fd36643
muhammaddiyor <muhammaddiyor20032808@gmail.com>
2024-08-01 10:07:44 +0500

    orders response

--- src/utils/api/url.js
+++ src/utils/api/url.js
@@ -1,6 +1,6 @@
 const SERVER_URL = "https://express-backend-9nif.onrender.com/";
-const LOCAL_URL = "http://localhost:5003/";
+// const LOCAL_URL = "http://localhost:5003/";
 
-const url = process.env.NODE_ENV === "production" ? SERVER_URL : LOCAL_URL;
+// const url = process.env.NODE_ENV === "production" ? SERVER_URL : LOCAL_URL;
 
-export default url;
+export default SERVER_URL;
